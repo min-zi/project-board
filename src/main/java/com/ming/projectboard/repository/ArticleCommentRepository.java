bdc318e443db25c2d7d742ab8349c5cbe52eccf4
min-zi <min-zi@naver.com>
2023-09-08 03:24:33 +0900

    #10 - repository interface 작성
    이걸로 db에 접근

---
+++ src/main/java/com/ming/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.ming.projectboard.repository;
+
+import com.ming.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
