cc26928ffeeaac2bd2c244c211534498b3ebce9b
min-zi <min-zi@naver.com>
2024-04-20 19:57:43 +0900

    #20 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/ming/projectboard/service/ArticleService.java
+++ src/main/java/com/ming/projectboard/service/ArticleService.java
@@ -1,15 +1,19 @@
 package com.ming.projectboard.service;
 
+import com.ming.projectboard.domain.Article;
 import com.ming.projectboard.domain.type.SearchType;
 import com.ming.projectboard.dto.ArticleDto;
 import com.ming.projectboard.dto.ArticleWithCommentsDto;
 import com.ming.projectboard.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -19,21 +23,48 @@ public class ArticleService {
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId)); // string 을 concatenation 방식
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.id());
+            // findById 대신 getReferenceById 를 사용하는 이유는
+            // 아이디가 있음을 아는 전제하에 바로 업데이트를 하고 싶지만, 엔티티를 일단 영속성 컨텍스트에서 가져와야 되니까 했음
+            // 근데 여기서 셀렉트 쿼리가 발생해버리기 때문에 레퍼런스만 가져오는 코드 getReferenceById 가 생김
+            if (dto.title() != null) { article.setTitle(dto.title()); }
+            if (dto.content() != null) { article.setContent(dto.content()); }
+            article.setHashtag(dto.hashtag());
+            // articleRepository.save(article);
+            // 따로 save 쿼리를 명시할 필요 없음, 트랜잭션이 끝날 때 영속성 컨텍스트는 article 이 변한 것을 감지하고 그 부분에 대해 쿼리가 실행이 됨
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto); // interpolation 방식
+        }
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 
 }
